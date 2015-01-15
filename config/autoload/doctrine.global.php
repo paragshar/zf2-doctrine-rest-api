057897cdf7c20b4a48025f3db8138ce06a969ab4
Parag Sharma <parag.shar@gmail.com>
2015-01-15 15:28:06 +0530

    adding all required properties in doctrine.global.php

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -6,7 +6,10 @@ return array(
         'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
         'params' => array(
           'host'     => 'localhost',
-          'port'     => '3306'
+          'port'     => '3306',
+          'user'     => 'root',
+          'password' => '',
+          'dbname'   => 'zf2tutorial',
         )
       )
     )
